16aa7041bd5d3791e18ad98dae2a53a9199c649a
Gabriel Horner <gabriel.horner@gmail.com>
2013-03-28 07:56:19 -0400

    add pedestal-service example

---
+++ examples/pedestal-service/README.md
@@ -0,0 +1,18 @@
+## Description
+
+An example of using emoji with pedestal-service
+
+## Getting Started
+
+1. Start the application: `lein run`
+2. Go to [localhost:8080](http://localhost:8080/)
+3. Read your app's source code at src/emoji/service.clj.
+4. Learn more about pedestal-service! See the [Links section below](#links).
+
+## Configuration
+
+To configure logging see config/logback.xml. By default, the app logs to stdout and logs/.
+To learn more about configuring Logback, read its [documentation](http://logback.qos.ch/documentation.html).
+
+## Links
+* [Other examples](https://github.com/pedestal/samples)
---
+++ examples/pedestal-service/src/emoji/service.clj
@@ -0,0 +1,38 @@
+(ns emoji.service
+    (:require [io.pedestal.service.http :as bootstrap]
+              [io.pedestal.service.http.route :as route]
+              [io.pedestal.service.http.body-params :as body-params]
+              [io.pedestal.service.http.route.definition :refer [defroutes]]
+              [io.pedestal.service.interceptor :refer [defon-response]]
+              [emoji.core :as emoji]
+              [ring.util.response :as ring-resp]))
+
+(defn home-page
+  [request]
+  {:status 200
+   :headers { "Content-Type" "text/html"}
+   :body "This page is on :fire:"})
+
+(defon-response emoji-interceptor
+  [response]
+  (emoji/emoji-response response))
+
+(defroutes routes
+  [[["/" {:get home-page} ^:interceptors [emoji-interceptor]]]])
+
+;; You can use this fn or a per-request fn via io.pedestal.service.http.route/url-for
+(def url-for (route/url-for-routes routes))
+
+;; Consumed by emoji.server/create-server
+(def service {:env :prod
+              ;; You can bring your own non-default interceptors. Make
+              ;; sure you include routing and set it up right for
+              ;; dev-mode. If you do, many other keys for configuring
+              ;; default interceptors will be ignored.
+              ;; :bootstrap/interceptors []
+              ::bootstrap/routes routes
+              ;; Root for resource interceptor that is available by default.
+              ::bootstrap/resource-path "/public"
+              ;; Choose from [:jetty :tomcat].
+              ::bootstrap/type :jetty
+              ::bootstrap/port 8080})
